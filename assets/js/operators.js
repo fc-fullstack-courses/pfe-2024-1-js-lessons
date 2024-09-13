a57b67b808719835e28c1e1ea2216b7b164b07e5
Kyrylo <fresh.js.ftd.@gmail.com>
2024-09-13 20:13:23 +0300

    Add basic math operators

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -9,3 +9,30 @@ console.log(typeof data);
 
 let bug = null;
 console.log(typeof bug); // object
+
+// Математичні оператори
+// +, -, *, /
+
+console.log('2 + 2');
+console.log(2 + 2);
+
+console.log('5 - 2');
+console.log(5 - 2);
+
+console.log('2 * 10');
+console.log(2 * 10);
+
+console.log('100 / 4');
+console.log(100 / 4);
+
+// зведення в ступінь
+console.log('5 ** 3');
+console.log(5 ** 3);
+
+console.log('-5 ** 2');
+console.log((-5) ** 2);
+
+// залишок від ділення 
+console.log('10 % 3');
+console.log(10 % 3);
+console.log(10 % -3);
