57f0be4b36ee106fcd5487087f1ff125a62b8567
Kyrylo <fresh.js.ftd.@gmail.com>
2024-09-16 20:27:45 +0300

    Add prompt function example usage

---
+++ assets/js/functions.js
@@ -0,0 +1,18 @@
+// Функції - блоки коду які можно повторно використовувати
+
+/*
+  параметри функції - речі які передаємо всередину 
+                      як додаткову інформацію 
+*/
+let text = 'test';
+console.log(text, 123);
+
+// let userName;
+
+// alert(userName);
+
+const userName = prompt('Enter your name');
+
+const greetingText = 'Hello ' + userName + '!';
+
+alert(greetingText);
