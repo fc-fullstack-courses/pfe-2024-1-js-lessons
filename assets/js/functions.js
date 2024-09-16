8b93c54e1695095cef3cfc01d5dc7cc04a700a20
Kyrylo <fresh.js.ftd.@gmail.com>
2024-09-16 20:51:23 +0300

    Add more prompt examples

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,15 +4,41 @@
   параметри функції - речі які передаємо всередину 
                       як додаткову інформацію 
 */
-let text = 'test';
-console.log(text, 123);
+// let text = 'test';
+// console.log(text, 123);
 
 // let userName;
 
 // alert(userName);
 
-const userName = prompt('Enter your name');
+// const userName = prompt('Enter your name');
 
-const greetingText = 'Hello ' + userName + '!';
+// const greetingText = 'Hello ' + userName + '!';
 
-alert(greetingText);
+// alert(greetingText);
+
+
+// const num1 = prompt("Enter first number");
+// const num2 = prompt("Enter second number");
+
+// alert(+num1 + +num2);
+
+
+// const num1 = +prompt("Enter first number");
+// const num2 = +prompt("Enter second number");
+
+// alert(num1 + num2);
+// alert(num1 + 50);
+// alert(num2 + 40005465);
+
+// const input1 = prompt("Enter first number");
+// const input2 = prompt("Enter second number");
+
+// const num1 = +input1;
+// const num2 = +input2;
+
+// alert(num1 + num2);
+
+const result = prompt();
+
+console.log(result);
