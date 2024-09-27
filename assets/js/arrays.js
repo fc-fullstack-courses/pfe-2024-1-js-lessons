c3d1ba0b18496e51f4c7cb104170916ae5d1b78c
Kyrylo <fresh.js.ftd.@gmail.com>
2024-09-27 19:30:59 +0300

    Commented out console logs

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -27,11 +27,11 @@ const vipEmails = new Array(
   'asdn4232@hotmail.com'
 );
 
-console.log(typeof vipEmails); // object
+// console.log(typeof vipEmails); // object
 
 // перевірка чи є об'єкт масивом
 const isVipEmailsArray = Array.isArray(vipEmails);
-console.log(isVipEmailsArray);
+// console.log(isVipEmailsArray);
 
 // console.log(vipEmails);
 // довжина масива
@@ -59,7 +59,7 @@ const arr4 = [];
 // масив з якимись елементами
 const arr5 = [50, 515, 8478, 4456];
 
-console.log(arr5[2]);
+// console.log(arr5[2]);
 
 arr5[4] = Infinity;
 
@@ -84,7 +84,7 @@ emptyArr.pop();
 emptyArr.pop();
 const popRes = emptyArr.pop();
 
-console.log(popRes); // undefined
+// console.log(popRes); // undefined
 
 /*
   1. у масив з іменами в кінець вставити 1 ім'я
@@ -97,28 +97,28 @@ const names = ['Anton', 'Olesya', 'Anna', 'Petro'];
 
 // 1
 names.push('John');
-console.log(names);
+// console.log(names);
 // 2
 names.push('Jane', 'Joshua');
-console.log(names);
+// console.log(names);
 
 // 3
 const deletedName = names.pop();
-console.log(deletedName);
-console.log(names);
+// console.log(deletedName);
+// console.log(names);
 
 // додавання елементів у початок масиву
-console.log(arr5);
+// console.log(arr5);
 
 arr5.unshift({
   test: true
 });
 
-console.log(arr5);
+// console.log(arr5);
 
 arr5.unshift(-1000, 'test 1235', null);
 
-console.log(arr5);
+// console.log(arr5);
 
 // додати на початок масиву з іменами 2 імені
 // стірі імена при цьому не мають зникнути
@@ -127,11 +127,11 @@ names.unshift('Test', 'User');
 
 // видалення першого елементу масиву
 const previousFirstElem = arr5.shift();
-console.log(arr5);
-console.log(previousFirstElem);
+// console.log(arr5);
+// console.log(previousFirstElem);
 
 // видалити перше ім'я з масиву та покажіть його у консолі
-console.log(names.shift());
+// console.log(names.shift());
 
 // видалення та вставка елементів у будь-яку точку масиву
 /*
@@ -140,10 +140,10 @@ console.log(names.shift());
   другий параметр - число, яке каже скільки елементів, починаючи з індексу треба видалити
   всі параметри після цього будуть вставлені на вказаний індекс після видалення елементів
 */
-console.log(arr5);
+// console.log(arr5);
 const deletedItemsArr = arr5.splice(-3, 1, 'splice sent me', 'hello there');
-console.log(arr5);
-console.log(deletedItemsArr);
+// console.log(arr5);
+// console.log(deletedItemsArr);
 
 
 // копіювання частини масиву у новий масив
@@ -163,7 +163,7 @@ const copy5 = arr5.slice(-4, 2);
 const copy6 = arr5.slice(1, 5);
 
 copy6[1].newProp = 'wifi';
-console.log(copy6[1] === arr5[2]);
+// console.log(copy6[1] === arr5[2]);
 
 // метод join
 /*
