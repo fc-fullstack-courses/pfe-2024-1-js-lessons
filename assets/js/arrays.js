4bce516093118638599cfedd4a71b2b0692fa431
Kyrylo <fresh.js.ftd.@gmail.com>
2024-09-27 19:29:56 +0300

    Add join example

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -164,3 +164,15 @@ const copy6 = arr5.slice(1, 5);
 
 copy6[1].newProp = 'wifi';
 console.log(copy6[1] === arr5[2]);
+
+// метод join
+/*
+  перетворює масив на рядок
+  елементи розмежуються або комами або 
+  рядком який передали як параметр
+*/
+
+const joinTest = ['Name 1', 'Name 2', true, 1000];
+
+const stringFromArr = joinTest.join();
+const stringFromArr2 = joinTest.join('===');
