eeac69c0f0fdc72b8a7da2919d08645fe9a44453
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-03 20:03:31 +0300

    Add advanced try ... catch example

--- assets/js/errors.js
+++ assets/js/errors.js
@@ -13,32 +13,55 @@
 // const test = 1;
 // test = 2;
 
-try {
-  // початок небезпечного коду
-  console.log(1);
+// try {
+//   // початок небезпечного коду
+//   console.log(1);
 
-  // const err1 = new TypeError('Приклад опису помилки');
+//   // const err1 = new TypeError('Приклад опису помилки');
 
-  // err1.status = 500;
+//   // err1.status = 500;
 
-  // throw err1;
+//   // throw err1;
 
-  // throw new TypeError('Приклад опису помилки');
+//   // throw new TypeError('Приклад опису помилки');
 
-  console.log(2);
-  // кінець небезпечного коду
-} catch (err) {
-  // блок коду який виконається якщо у блоці try відбулася помилка
-  console.log('помилка сталася');
+//   console.log(2);
+//   // кінець небезпечного коду
+// } catch (err) {
+//   // блок коду який виконається якщо у блоці try відбулася помилка
+//   console.log('помилка сталася');
 
-  console.dir(err);
+//   console.dir(err);
 
-  if(err.message === 'Приклад опису помилки') {
-    console.log('обробив помилку');
+//   if(err.message === 'Приклад опису помилки') {
+//     console.log('обробив помилку');
+//   }
+// } finally {
+//   // блок коду який виконується або після catch або після повного виконання try
+//   console.log('finally');
+// }
+
+// console.log('весь код виконано');
+
+function getSumOfTwoNumbers(num1, num2) {
+  if (typeof num1 !== 'number' || isNaN(num1)) {
+    throw new TypeError('num1 має бути корректним числом');
   }
-} finally {
-  // блок коду який виконується або після catch або після повного виконання try
-  console.log('finally');
+
+  if (typeof num2 !== 'number' || isNaN(num2)) {
+    throw new TypeError('num2 має бути корректним числом');
+  }
+
+  return num1 + num2;
+}
+
+try {
+  const res = getSumOfTwoNumbers('2', 2);
+
+  console.log(res);
+  
+} catch (err) {
+  console.log(err.message);
 }
 
 console.log('весь код виконано');
