7dc88e99469c7fe7ad45706f3dc42deafecdf1b5
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-03 20:46:00 +0300

    Add type conversion through functions example

--- assets/js/primitives.js
+++ assets/js/primitives.js
@@ -8,14 +8,14 @@
 // console.log(textFromServer);
 // console.log(textFromServer.length);
 
-const str1 = 'test';
-console.log(str1);
+// const str1 = 'test';
+// console.log(str1);
 
-const str2 = new String('test');
-console.log(str2);
+// const str2 = new String('test');
+// console.log(str2);
 
-const arr1 = ['t', 'e', 's', 't'];
-console.log(arr1);
+// const arr1 = ['t', 'e', 's', 't'];
+// console.log(arr1);
 
 // const bool1 = false;
 
@@ -25,10 +25,32 @@ console.log(arr1);
 //   console.log(2);
 // }
 
-const bool2 = new Boolean(false);
+// const bool2 = new Boolean(false);
 
-if (bool2) {
-  console.log(1);
-} else {
-  console.log(2);
-}
+// if (bool2) {
+//   console.log(1);
+// } else {
+//   console.log(2);
+// }
+
+// Використання функцій Boolean, String, Number
+const test1 = Number(10);
+console.log(test1);
+
+const test2 = Number('10');
+console.log(test2);
+
+// +data
+const test3 = Number('eteasfdfdfd');
+console.log(test3);
+
+// console.log(+'eteasfdfdfd');
+
+// !!data
+const bool1 = Boolean({
+});
+console.log(bool1);
+
+// '' + data
+const str1 = String(null);
+console.log(str1);
