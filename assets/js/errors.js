505c05f864d26b1a74b841369311d157de5395bd
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-03 19:50:22 +0300

    Add basic try ... catch example

--- assets/js/errors.js
+++ assets/js/errors.js
@@ -13,16 +13,32 @@
 // const test = 1;
 // test = 2;
 
-console.log(1);
+try {
+  // початок небезпечного коду
+  console.log(1);
 
-// const err1 = new TypeError('Приклад опису помилки');
+  // const err1 = new TypeError('Приклад опису помилки');
 
-// err1.status = 500;
+  // err1.status = 500;
 
-// console.log(err1.message);
+  // throw err1;
 
-// throw err1;
+  // throw new TypeError('Приклад опису помилки');
 
-throw new TypeError('Приклад опису помилки');
+  console.log(2);
+  // кінець небезпечного коду
+} catch (err) {
+  // блок коду який виконається якщо у блоці try відбулася помилка
+  console.log('помилка сталася');
 
-console.log(2);
+  console.dir(err);
+
+  if(err.message === 'Приклад опису помилки') {
+    console.log('обробив помилку');
+  }
+} finally {
+  // блок коду який виконується або після catch або після повного виконання try
+  console.log('finally');
+}
+
+console.log('весь код виконано');
