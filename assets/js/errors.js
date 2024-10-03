5be454950a4f28a741f8d8cadd2e6082d887517f
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-03 19:09:31 +0300

    Add errors and error throwing example

---
+++ assets/js/errors.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// console.log(1);
+
+// console.log(2);
+
+// console.log(user1);
+
+// console.log(3);
+
+// if () {}
+
+// const test = 1;
+// test = 2;
+
+console.log(1);
+
+// const err1 = new TypeError('Приклад опису помилки');
+
+// err1.status = 500;
+
+// console.log(err1.message);
+
+// throw err1;
+
+throw new TypeError('Приклад опису помилки');
+
+console.log(2);
