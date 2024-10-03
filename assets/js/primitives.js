b846b7ea6272187d7cea1c477c24bc5700719a44
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-03 20:37:06 +0300

    Add primitive properties examples

---
+++ assets/js/primitives.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// console.log('test text');
+// console.log('test text'.length);
+
+// const textFromServer = 'test text';
+
+// console.log(textFromServer);
+// console.log(textFromServer.length);
+
+const str1 = 'test';
+console.log(str1);
+
+const str2 = new String('test');
+console.log(str2);
+
+const arr1 = ['t', 'e', 's', 't'];
+console.log(arr1);
+
+// const bool1 = false;
+
+// if(bool1) {
+//   console.log(1);
+// } else {
+//   console.log(2);
+// }
+
+const bool2 = new Boolean(false);
+
+if (bool2) {
+  console.log(1);
+} else {
+  console.log(2);
+}
