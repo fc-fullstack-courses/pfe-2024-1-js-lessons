c92623e01ce111bc56f14f0a95228aba6ab7148f
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-10 19:12:26 +0300

    Add spread operator examples

--- assets/js/rest-and-spread.js
+++ assets/js/rest-and-spread.js
@@ -1,11 +1,11 @@
 // об'єкт arguments
 /**
  * Функція суми необмеженої кількості значень
- * @param {number} num1 
- * @param {number} num2 
+ * @param {number} num1
+ * @param {number} num2
  * @returns {number}
  */
-function sum (num1, num2) {
+function sum(num1, num2) {
   console.log(num1);
   console.log(num2);
 
@@ -15,7 +15,7 @@ function sum (num1, num2) {
 
   let resultingSum = num1 + num2;
 
-  for(let i = 0; i < arguments.length; i++) {
+  for (let i = 0; i < arguments.length; i++) {
     resultingSum += arguments[i];
   }
 
@@ -35,12 +35,11 @@ function sum (num1, num2) {
 
 // console.dir(arrow);
 
-
 // rest-оператор (оператор залишкових параметрів)
 /**
  * Функція суми необмеженої кількості значень
- * @param {number} num1 
- * @param {number} num2 
+ * @param {number} num1
+ * @param {number} num2
  * @param {...number} numbers - масив аргументів - чисел починаючи з третього аргументу
  * @param {number[]} numbers - масив аргументів - чисел починаючи з третього аргументу
  * @returns {number}
@@ -53,9 +52,9 @@ const sumWithRestOperator = (num1, num2, ...numbers) => {
   });
 
   return resultingSum;
-}
+};
 
-console.log(sumWithRestOperator(1,2,3));
+// console.log(sumWithRestOperator(1, 2, 3));
 
 const sumWithRestOperator2 = (...numbers) => {
   let resultingSum = 0;
@@ -65,4 +64,32 @@ const sumWithRestOperator2 = (...numbers) => {
   });
 
   return resultingSum;
-}
+};
+
+// spread - оператор (оператор розширення)
+
+const numbers = [20, 35, 15, 7, 2, -10, 0, 100, -0.5];
+
+// const minNumber = Math.min(
+//   numbers[0],
+//   numbers[1],
+//   numbers[2],
+//   numbers[3],
+//   numbers[4],
+//   numbers[5],
+//   numbers[6],
+//   numbers[7],
+//   numbers[8],
+//   10000
+// );
+// console.log(minNumber);
+
+const minNumber = Math.min(...numbers, 10000);
+console.log(minNumber);
+
+const nums1 = [2,4,6,8,10];
+const nums2 = [1,3,5,7,9];
+
+const allNums = [...nums1, 1000000000, ...nums2, -1000];
+
+const sumOfAllNums = sumWithRestOperator2(...allNums);
