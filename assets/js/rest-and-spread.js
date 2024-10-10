f91f855cb4867185fb931ccf0f23457afd6fa4c3
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-10 18:49:08 +0300

    Add rest operator

--- assets/js/rest-and-spread.js
+++ assets/js/rest-and-spread.js
@@ -27,10 +27,42 @@ function sum (num1, num2) {
 // приймає необмежену кількість аргументів
 // arr1.push();
 
-console.log(sum(1,2,3));
+// console.log(sum(1,2,3));
 
 // console.dir(sum);
 
 // const arrow = () => {};
 
 // console.dir(arrow);
+
+
+// rest-оператор (оператор залишкових параметрів)
+/**
+ * Функція суми необмеженої кількості значень
+ * @param {number} num1 
+ * @param {number} num2 
+ * @param {...number} numbers - масив аргументів - чисел починаючи з третього аргументу
+ * @param {number[]} numbers - масив аргументів - чисел починаючи з третього аргументу
+ * @returns {number}
+ */
+const sumWithRestOperator = (num1, num2, ...numbers) => {
+  let resultingSum = num1 + num2;
+
+  numbers.forEach((number) => {
+    resultingSum += number;
+  });
+
+  return resultingSum;
+}
+
+console.log(sumWithRestOperator(1,2,3));
+
+const sumWithRestOperator2 = (...numbers) => {
+  let resultingSum = 0;
+
+  numbers.forEach((number) => {
+    resultingSum += number;
+  });
+
+  return resultingSum;
+}
