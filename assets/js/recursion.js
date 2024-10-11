2ade3376b7c58ebc94899db1187f140e83dd83c1
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-11 20:31:59 +0300

    Add binary tree reduce recursive function

--- assets/js/recursion.js
+++ assets/js/recursion.js
@@ -50,3 +50,59 @@ countdown(5);
     2. якась з запуссків функцій доходить до базового випадку (не запускаї іншу функцію до свого завершення)
     3. всі інші функці завершують свою роботу
 */
+// total: 51
+const tree = {
+  value: 2,
+  left: {
+    value: 7,
+    left: {
+      value: 2
+    },
+    right: {
+      value: 6,
+      left: {
+        value: 5
+      },
+      right: {
+        value: 11
+      }
+    }
+  },
+  right: {
+    value: 5,
+    right: {
+      value: 9,
+      left: {
+        value: 4
+      }
+    }
+  }
+}
+
+/**
+ * Функція для обходу та отримання суми двійкового дерева
+ * @param {object} tree об'єкт дерева
+ * @param {number} tree.value значення вершини дерева
+ * @param {object} [tree.left] ліва гілка
+ * @param {object} [tree.right] права гілка
+ * @returns {number} сума вершин дерева
+ */
+function reduceTree (tree) {
+  let result = tree.value;
+
+  // якщо є ліва гілка треба розрахувати її суму і додати до result
+  if(tree.left) {
+    result += reduceTree(tree.left);
+  }
+
+  // якщо є права гілка треба розрахувати її суму і додати до result
+  if(tree.right) {
+    result += reduceTree(tree.right);
+  }
+
+  // базовий випадок
+  return result;
+}
+
+// debugger;
+// reduceTree(tree);
