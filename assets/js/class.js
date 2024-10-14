c396252e6740b560a42c4a69e5d0f2d341b59b68
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-14 18:43:34 +0300

    Add basic class example

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,7 +1,12 @@
 /**
- * 
+ *
  */
-function OldHouse (floors = 1, apartmentsPerFloor = 4, address = 'DEFAULT ADDRESS, CHANGE IT', priceOfApartment = 15000) {
+function OldHouse(
+  floors = 1,
+  apartmentsPerFloor = 4,
+  address = 'DEFAULT ADDRESS, CHANGE IT',
+  priceOfApartment = 15000
+) {
   this.floors = floors;
   this.apartmentsPerFloor = apartmentsPerFloor;
   this.address = address;
@@ -10,22 +15,44 @@ function OldHouse (floors = 1, apartmentsPerFloor = 4, address = 'DEFAULT ADDRES
   // this.getFullHouseData = function () {
   //   return `This house address: ${this.address}, has ${this.floors} floors with ${this.apartmentsPerFloor} apartments on each floor. Each apartment costs ${this.priceOfApartment}`;
   // }
-}// const housePrototype = {
+} // const housePrototype = {
 //   getFullHouseData : function () {
 //     return `This house address: ${this.address}, has ${this.floors} floors with ${this.apartmentsPerFloor} apartments on each floor. Each apartment costs ${this.priceOfApartment}`;
 //   }
 // }
 
-function HousePrototype () {
+function HousePrototype() {
   this.getFullHouseData = function () {
     return `This house address: ${this.address}, has ${this.floors} floors with ${this.apartmentsPerFloor} apartments on each floor. Each apartment costs ${this.priceOfApartment}`;
-  }
+  };
 }
 
 const housePrototype = new HousePrototype();
 
-const house1 = new OldHouse(3,4,'test street 1', 1200);
+const house1 = new OldHouse(3, 4, 'test street 1', 1200);
 
-House.prototype = housePrototype;
+OldHouse.prototype = housePrototype;
 
 const house2 = new OldHouse();
+
+// клас - новий синтаксис написання функцій-конструкторів
+class House {
+  constructor(
+    floors = 1,
+    apartmentsPerFloor = 4,
+    address = 'DEFAULT ADDRESS, CHANGE IT',
+    priceOfApartment = 15000
+  ) {
+    this.floors = floors;
+    this.apartmentsPerFloor = apartmentsPerFloor;
+    this.address = address;
+    this.priceOfApartment = priceOfApartment;
+  }
+
+  getFullHouseData () {
+    return `This house address: ${this.address}, has ${this.floors} floors with ${this.apartmentsPerFloor} apartments on each floor. Each apartment costs ${this.priceOfApartment}`;
+  }
+}
+
+const house3 = new House();
+const house4 = new House(4,4, 'test address 2', 25000);
