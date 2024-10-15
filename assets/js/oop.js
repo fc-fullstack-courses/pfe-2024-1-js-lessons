4dfc57cf03e3fd0c4cd65cffffe18e1ef4488079
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-15 20:15:31 +0300

    Add basic oop inheritance example

---
+++ assets/js/oop.js
@@ -0,0 +1,61 @@
+// Концепції ООП (об'єктно-орієнтованого програмування)
+
+// 1. Успадкування
+class Animal {
+  constructor (nickname, weight = 1, type = 'animal') {
+    this.nickname = nickname;
+    this.weight = weight;
+    this.type = type;
+  }
+
+  sleep () {
+    console.log(`${this.nickname} is sleeping`);
+  }
+
+  eat () {
+    console.log(`${this.nickname} is eating`);
+  }
+}
+
+const mouse = new Animal('Jerry');
+const dog = new Animal('Bulldog');
+
+// class Bird {
+//   constructor (nickname, weight = 1) {
+//     this.nickname = nickname;
+//     this.weight = weight;
+//   }
+
+//   sleep () {
+//     console.log(`${this.nickname} is sleeping`);
+//   }
+
+//   eat () {
+//     console.log(`${this.nickname} is eating`);
+//   }
+
+//   fly () {
+//     console.log(`${this.nickname} is flying`);
+//   }
+// }
+
+
+class Bird extends Animal {
+  constructor (nickname, weight = 1, wingspan = 0.7) {
+    // запуск батьківського конструктора ( зараз Animal)
+    // має бути запущений до роботи с this
+    // та до повертання підсумкового об'єкту
+    super(nickname, weight, 'bird');
+
+    // this.nickname = nickname;
+
+    this.wingspan = wingspan;
+  }
+
+  fly () {
+    console.log(`${this.nickname} is flying`);
+  }
+}
+
+const parrot1 = new Bird('Kesha', 2, 0.8);
+const parrot2 = new Bird('NotKesha', 0.5, 1);
