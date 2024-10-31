dec00640e5163ce6010d6f580c7e5f097c72889b
Kyrylo <fresh.js.ftd.@gmail.com>
2024-10-31 20:41:49 +0200

    Add object destructuring examples

--- assets/js/destructurisation.js
+++ assets/js/destructurisation.js
@@ -28,8 +28,67 @@ console.log(monitor.sizes.width.value);
 
 // створити функцію, яка рахує діагональ монітора
 function calculateDiagonal(monitor) {
-  const width = monitor.sizes.width.value;
-  const height = monitor.sizes.height.value;
+  // const width = monitor.sizes.width.value;
+  // const height = monitor.sizes.height.value;
+  const {
+    sizes: {
+      width: { value: width },
+      height: { value: height },
+    },
+  } = monitor;
 
   return Math.sqrt(width * width + height * height);
 }
+
+const car = {
+  price: 51651651516,
+  model: 'XL-456',
+};
+
+const model = car.model;
+
+// деструктуризація об'єктів
+
+const {
+  color,
+  price,
+  model: monitorModel,
+  sizes: {
+    width: { value: width },
+  },
+  sizes,
+} = monitor;
+
+// const color = monitor.color;
+// const monitorModel = monitor.model;
+
+// const width = monitor.sizes.width.value;
+
+console.log(color); // === monitor.color
+console.log(price); // 9999.99
+console.log(monitorModel); // 321asd131rgfr
+
+// bad
+// const { sizes } = monitor;
+// const { width: monitorWidthObj } = sizes;
+// const { value: width } = monitorWidthObj;
+
+// good
+// const {
+//   sizes: {
+//     width: { value: width },
+//   },
+// } = monitor;
+console.log(width); // 68
+
+const user = {
+  id: 24,
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johnDoe123@gmail.com',
+  password: '12345admin'
+}
+
+// rest у деструктуризації об'єкта створює новий об'єкт 
+// в якому будуть усі недеструктуровані дані 
+const { password, ...userWithoutPassword } = user;
